f9ae74531f6c755f2a87c7220a1a19cff447ff17
OliviaYJH <0703olivia@naver.com>
2023-08-15 23:05:02 +0900

    [feat] my participation detail UI

---
+++ app/src/main/java/com/example/habday_android/src/main/list/myparticipation/detail/MyParticipationDetailFundingActivity.kt
@@ -0,0 +1,14 @@
+package com.example.habday_android.src.main.list.myparticipation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.habday_android.config.BaseActivity
+import com.example.habday_android.databinding.ActivityMyParticipationDetailFundingBinding
+
+class MyParticipationDetailFundingActivity : BaseActivity<ActivityMyParticipationDetailFundingBinding>(ActivityMyParticipationDetailFundingBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}

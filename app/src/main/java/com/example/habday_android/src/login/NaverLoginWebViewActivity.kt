280f8202b417c29c4cb17c42107e322714b57ef2
OliviaYJH <0703olivia@naver.com>
2023-09-04 17:20:19 +0900

    [fix] 소셜 로그인 코드

--- app/src/main/java/com/example/habday_android/src/login/NaverLoginWebViewActivity.kt
+++ app/src/main/java/com/example/habday_android/src/login/NaverLoginWebViewActivity.kt
@@ -9,16 +9,21 @@ import android.webkit.WebBackForwardList
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.example.habday_android.R
+import com.example.habday_android.config.ApplicationClass
+import com.example.habday_android.config.ApplicationClass.Companion.editor
 import com.example.habday_android.config.BaseActivity
 import com.example.habday_android.databinding.ActivityNaverLoginWebViewBinding
 import com.example.habday_android.src.login.addinfo.AddInformationActivity
+import com.example.habday_android.src.login.addinfo.getToken.GetTokenResponse
+import com.example.habday_android.src.login.addinfo.getToken.GetTokenService
+import com.example.habday_android.src.login.addinfo.getToken.GetTokenView
 import com.example.habday_android.src.login.checkMember.CheckMemberResponse
 import com.example.habday_android.src.login.checkMember.CheckMemberService
 import com.example.habday_android.src.login.checkMember.CheckMemberView
 import com.example.habday_android.src.main.MainActivity
 
-class NaverLoginWebViewActivity : BaseActivity<ActivityNaverLoginWebViewBinding>(ActivityNaverLoginWebViewBinding::inflate), CheckMemberView {
-    var flag = true
+class NaverLoginWebViewActivity : BaseActivity<ActivityNaverLoginWebViewBinding>(ActivityNaverLoginWebViewBinding::inflate), CheckMemberView, GetTokenView {
+    var flag = false
     var nowUrl : String ?= null
     var code : String? = null
 
@@ -45,9 +50,9 @@ class NaverLoginWebViewActivity : BaseActivity<ActivityNaverLoginWebViewBinding>
             super.onPageFinished(view, url)
             nowUrl = binding.webViewNaverLogin.url
 
-            //Log.d("okhttp_nowUrl", nowUrl.toString())
+            Log.d("okhttp_nowUrl", nowUrl.toString())
             code = nowUrl?.substring(nowUrl.toString().length - 18)
-            //Log.d("okhttp_code", code.toString())
+            Log.d("okhttp_code", code.toString())
 
             // api 호출
             CheckMemberService(this@NaverLoginWebViewActivity).tryGetCheckMember()
@@ -70,8 +75,11 @@ class NaverLoginWebViewActivity : BaseActivity<ActivityNaverLoginWebViewBinding>
 
     override fun getCheckMemberSuccess(response: CheckMemberResponse) {
         // 이미 가입된 사용자
-        startActivity(Intent(this@NaverLoginWebViewActivity, MainActivity::class.java))
-        finish()
+        // 토큰 발급 받은 후 성공하면 메인으로
+        if(!code?.startsWith("response")!!){
+            Log.d("okhttp_member_code", "success")
+            GetTokenService(this).tryGetToken(code!!)
+        }
     }
 
     override fun getCheckMemberFailure(message: String) {
@@ -80,6 +88,21 @@ class NaverLoginWebViewActivity : BaseActivity<ActivityNaverLoginWebViewBinding>
         intent.putExtra("code", code)
         startActivity(intent)
         finish()
+    }
+
+    override fun getTokenSuccess(response: GetTokenResponse) {
+        // 토큰 발급 성공
+        editor.putString("accessToken", response.accessToken)
+        editor.commit()
+
+        intent = Intent(this@NaverLoginWebViewActivity, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun getTokenFailure(message: String) {
+        showCustomToast("토큰 발급에 실패했습니다")
     }
 
 
